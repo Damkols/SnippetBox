466941cfbe5a55e350a913fdbac81bba30598c60
Kols <damolausmankolapo@gmail.com>
2025-05-03 15:34:25 +0100

    created a snippetView handler

--- main.go
+++ main.go
@@ -6,14 +6,18 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippet Box"))
+	w.Write([]byte("Hello from Snippet Box")) //--> Displays the text on home page
+}
+
+func snippetView(w htttp.ResponseWriter, r *http.Request) {
+	w.Write([]byte ("Display a specific snippet")) //--> Display a specific snippet
 }
 
 func main() {
 
 	mux:= http.NewServeMux() //--> creates a routing system 
 
-	mux.HandleFunc("/", home) //--> maps a particular path to a soecific route
+	mux.HandleFunc("/", home) //--> maps a particular path to a specific route
 
 	log.Print("starting server on :4000") //--> log starting server on port :4000 to the terminal
 
