ad5fa308011dc50ecf282005b64edd501b0c0616
Kols <damolausmankolapo@gmail.com>
2025-05-04 14:57:44 +0100

    displayed content on home page succesfully

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,20 +4,29 @@ import (
     "strconv"
     "fmt"
     "net/http"
+	"html/template"
+	"log"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 
     w.Header().Add("Server", "Go") //--> setting response header map, header name: Server, header value: Go
 
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html") //--> Parse home tmpl file
 
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w,"Internal Server Error", http.StatusInternalServerError)
-	}
-	
-    w.Write([]byte("Hello from Snippet Box")) //--> Displays the text on home page
+		return
+	} //-->  catch error if any
+
+	err = ts.Execute(w, nil) //--> execute parsed tmpl file
+
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	} //--> catch error if any
+
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
