f75dfe798334e9d30c99f33f797e861924f8a67a
Kols <damolausmankolapo@gmail.com>
2025-05-05 21:15:37 +0100

    modified home function to a method of type application

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,7 @@ import (
 	"log"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) { //--> method of type application
 
     w.Header().Add("Server", "Go") //--> setting response header map, header name: Server, header value: Go
 
