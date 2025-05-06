c299747105bcd9789f8e9d89321bda50990cd45c
Kols <damolausmankolapo@gmail.com>
2025-05-06 22:20:39 +0100

    imports and error fixes

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"net/http"
+	"runtime/debug"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	var (
 		method = r.Method
 		uri = r.URL.RequestURI()
-		trace = string(debug.Stack)
+		trace = string(debug.Stack())
 	)
 
 	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
