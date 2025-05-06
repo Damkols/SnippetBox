61a9ef4f990292f06e996312d018f0f82fc69181
Kols <damolausmankolapo@gmail.com>
2025-05-06 22:10:13 +0100

    using statusText to return human friendly text for a status code

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -11,5 +11,7 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 	)
 
 	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 
 }
