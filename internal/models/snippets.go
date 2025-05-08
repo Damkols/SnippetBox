5cd50014c7eb027d35d2a369b9499395383309ed
Kols <damolausmankolapo@gmail.com>
2025-05-08 14:37:49 +0100

    eror fixes

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -21,7 +21,7 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 
 	stmt := `INSERT INTO snippets (title, content, created, expires) VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))` //--> SQL statement to insert data to our database
 
-	db, err := m.DB.Exec(stmt, title, content, expires) //--> use Exec method to execute statement
+	result, err := m.DB.Exec(stmt, title, content, expires) //--> use Exec method to execute statement
 
 	if err != nil {
 		return 0, err
