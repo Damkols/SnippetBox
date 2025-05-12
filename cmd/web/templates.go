5e65e39466a752e49e1a2058f945a491dab6f9a6
Kols <damolausmankolapo@gmail.com>
2025-05-12 20:18:15 +0100

    error handling

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -16,5 +16,9 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{} //--> Initialize a new map to act as cache
 
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html") //--> get all tmpl.html files in pages directory
+	if err != nil {
+		return nil, err
+	}
+
 	
 }
