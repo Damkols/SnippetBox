a2710c5c378e6e015334c78dbbd49dc2e61af7de
Kols <damolausmankolapo@gmail.com>
2025-05-15 15:01:26 +0100

    wrap existing chain with logRequest middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,6 @@ func (app *application) routes() http.Handler {
 
     mux.HandleFunc("POST /snippet/create", app.snippetCreate) // --> maps /snippet/create to snippetCreate handler
 
-	return commonHeaders(mux)
+	return app.logRequest(commonHeaders(mux))
 
 }
