5f8ccfc6abe37ed8e70347d111b94c7945bd53ac
Kols <damolausmankolapo@gmail.com>
2025-05-15 15:19:37 +0100

    recover panic middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -32,3 +32,17 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 		next.ServeHTTP(w,r)
 	})
 }
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandleFunc(func(w http.ResponseWriter, r *http.Request){
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+
+				app.serverError(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
